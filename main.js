7850e08d140590d0784bb0e8c468b08288d001c6
luyvannda <philips.joe329@gmail.com>
2023-08-01 15:04:19 +0700

    change .container to .board and use DOM manipulation to style it

---
+++ main.js
@@ -0,0 +1,5 @@
+const board = document.querySelector(".board");
+board.style.width = "31.25rem";
+board.style.height = "31.25rem";
+board.style.display = "grid";
+board.style.border = "1px solid black";
